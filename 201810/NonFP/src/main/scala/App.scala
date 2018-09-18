4f9406e9c22f374ff35df672c619cb4037dacbee
Binzi Cao <binzic@campainmonitor.com>
2018-09-18 14:13:54 +1000

    sample

--- 201810/NonFP/src/main/scala/App.scala
+++ 201810/NonFP/src/main/scala/App.scala
@@ -1,10 +1,17 @@
 object MyApp extends App {
 
-  def deleteUser(id: String) = {
+  def deleteUser(id: String): Boolean = {
     Log.debug(s"Delete user ${id}")
-    val db     = DB("jdbc:postgres://locahost:5432/user_db")
-    val result = db.runQuery(s"DELETE FROM user_table WHERE id = ${id}")
-    Log.debug(s"Delete user result ${id}, ${result}")
+    val db = DB("jdbc:postgres://locahost:5432/user_db")
+    val result = db.runQuery(
+      s"""
+      DELETE FROM 
+          user_table 
+      WHERE 
+          id = ${id}
+      """
+    )
+    Log.debug(s"Delete user ${id}, return ${result}")
     result
   }
 
