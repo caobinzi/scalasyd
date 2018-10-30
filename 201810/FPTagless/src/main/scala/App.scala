c6b7519a5446e03e2b65e7647a46422db32ad707
Binzi Cao <binzic@campainmonitor.com>
2018-10-30 18:36:12 +1100

    to eff

--- 201810/FPTagless/src/main/scala/App.scala
+++ 201810/FPTagless/src/main/scala/App.scala
@@ -1,17 +1,24 @@
+import org.atnos.eff._
+import org.atnos.eff.all._
+import org.atnos.eff.syntax.all._
+import org.atnos.eff.addon.cats.effect.IOEffect._
+import org.atnos.eff.syntax.addon.cats.effect._
+
 import cats._
 import cats.data._
-import cats.implicits._
+
 import cats.instances._
 
 object MyApp extends App {
   import scala.concurrent.Future
   import scala.concurrent.ExecutionContext.Implicits.global
 
-  def program[F[_]: Monad](
+  def program[F[_]: Monad, R: F |= ?](
       gdpr: GdprOp[F],
       user: UserOp[F],
       log:  LogOp[F]
-  ): F[Unit] = {
+  ): Eff[R, Unit] = {
+    import EffHelper._
     for {
       data   <- gdpr.deleteUserEmail("testuser@abcd.com")
       _      <- log.info(s"Found ${data.info}")
@@ -19,14 +26,24 @@ object MyApp extends App {
       _      <- log.info(s"Finished Delete")
     } yield ()
   }
-  program(
-    GdprIter,
-    UserIter,
-    LogIter
-  )
+  type Stack = Fx.fx1[Future]
+
+  {
+    import cats.implicits._
+
+    import scala.concurrent._, duration._
+    implicit val scheduler = ExecutorServices.schedulerFromGlobalExecutionContext
+    import org.atnos.eff.syntax.future._
+    program[Future, Stack](
+      GdprIter,
+      UserIter,
+      LogIter
+    )
+//    Await.result(result.runSequential, 1 second)
+  }
 
   println("Getting here")
-  Thread.sleep(1000000)
+//  Thread.sleep(1000000)
 
   //   .runFuture
 
